cc3a83e2e9303a788c22dd7e2af6bbfeeae9a758
ENK Bond <43937176+ENKBond@users.noreply.github.com>
2018-11-19 23:23:18 -0700

    updates with css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,20 @@
-//main words to guess
-var selectWord = [
-    "shakespeare",
-    "austen",
-    "dostoyevsky",
-    "twain",
-    "woolfe",
-    "dickens",
-    "salinger",
-    "bronte",
-    "steinbeck",
-    "hemingway",
-];
-
-//10 tries set
-var maxTries = 10;
-
-//variables
 var guessedLetters = [];
 var guessingWord = [];
-var remainingGuesses = 0;
+var remainingGuesses = 10;
 var wins = 0;
-
-//press any key to start the game
+var loss = 0;
+var maxTries = 10;
+var selectWord = ["orwell", "austen", "tolstoy", "twain", "woolf","dickens", "salinger","bronte", "steinbeck", "hemingway"];
+var lettersGuessed;
+var lettersNoMatch;
 
 document.onkeyup = function gameStart (event) {
     var word = selectWord[Math.floor(Math.random()*selectWord.length)];
     for (var i = 0; i < word.length; i++ ) {
         guessingWord[i] = "_";
         document.getElementById("guessTheWord").innerText = guessingWord.join(" ");
-    };
-    document.getElementById("numberLeft").innerText = maxTries;
+    }
+    document.getElementById("numberLeft").innerText = remainingGuesses;
     document.getElementById("numberWins").innerText = wins;
-    document.getElementById("start").style.visibility = "hidden";
-    document.getElementById("")
+    document.getElementById("numberLoss").innerText = loss;
 };
-
-if (remainingGuesses > 0) {
-    document.onkeyup = function makeGuess (event) {
-        if (event.keyCode >= 65 && event.keyCode <= 90) {
-         pickLetter(event.key.toLocaleLowerCase());
-    }
-        function pickLetter(letter) {
-            if (guessedLetters.indexOf(letter) === -1) {
-                guessedLetters.push(letter);
-        }
-    }
-}
-}
